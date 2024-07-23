227b9bf30ee34a96af6986cb65b77ff3f184aa75
Isuu1 <jakub.babelek@gmail.com>
2024-07-23 13:42:48 +0100

    Added and configured Helmet library

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-import express from 'express';
+import express from "express";
+import helmet from "helmet";
+
 const app = express();
 const PORT = 3000;
 
-app.get ('/', (req, res) => {
-res.send ("Hello World!")
+// Use Helmet!
+app.use(helmet());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-  });
+  console.log("Server is running on http://localhost:3000");
+});

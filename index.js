d7d415cf33d32a07f63d6b6743cb07eea25d6795
James O'Kane <orbiccode@gmail.com>
2024-07-24 13:56:17 +0100

    Epic 2 User Story 1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import helmet from "helmet";
-import data from "./activities.js";
+import data from "./activities.json" with { type: "json" }
 
 // const sample = require('./sample.json');
 
@@ -14,7 +14,15 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// app.get();
+app.get("/activities", (req, res) => {
+  try {
+  res.status(200).json({"status": true, data})
+} catch(error) {
+  console.error(error);
+  res.status(400).send('Failed to fetch data')
+}
+
+})
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");

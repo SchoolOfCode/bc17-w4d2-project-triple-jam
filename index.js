e2bd2e3fc5c93033f3ae7ab24f70a6568f2e2b18
Isuu1 <jakub.babelek@gmail.com>
2024-07-26 14:14:38 +0100

    Refactoring

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ const PORT = 3000;
 app.use(helmet());
 app.use(express.json());
 
+//Check if server sends a response
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// GET handler
+// GET activities array
 app.get("/activities", (req, res) => {
   try {
     res.status(200).json({ status: "success", data });
@@ -27,20 +28,20 @@ app.get("/activities", (req, res) => {
 
 // POST handler
 app.post("/activities", (req, res) => {
+  //Grab new activity from body request
   const activity = req.body.newActivity;
   // If no body is provided send response code 400
   if (!activity) {
-    res.status(400).send("No data");
+    res.status(400).send("Please provide object body");
   }
   //Creating new object and adding 2 new properties
   const newActivity = {
-    ...activity,
     id: crypto.randomUUID(),
-    date: Date.now(),
+    activity_submitted: Date.now(),
+    ...activity,
   };
   //Push new object to activities array
   data.push(newActivity);
-  console.log(data);
   res.status(200).send({ status: "success", data });
 });
 
@@ -48,66 +49,36 @@ app.post("/activities", (req, res) => {
 app.put("/activities/:id", (req, res) => {
   const updateBody = req.body.update;
   const id = req.params.id;
-  const index = data.findIndex(activity => activity.id === id);
-if (!id) {
-  res.status(400).send('Bad request')
-  return
-}
-const updatedActivity = {
-    "id":id,
-    "activity_submitted": Date.now(),
-    ...updateBody
+  const index = data.findIndex((activity) => activity.id === id);
+  // -1 is a value returned from findIndex method
+  if (index === -1) {
+    res.status(400).send("ID does not exist");
+    return;
   }
-
+  //Create updated object
+  const updatedActivity = {
+    id: id,
+    activity_submitted: Date.now(),
+    ...updateBody,
+  };
+  //Replacing object in array
   data[index] = updatedActivity;
 
-  res.status(200).send( {status: "success", data})
+  res.status(200).send({ status: "success", data });
 });
-
-
-
-// // DELETE handler
-// app.delete("/activities/:id", (req, res) => {
-//   // const deleteBody = req.body.delete
-//   const id = req.params.id;
-//   const index = data.findIndex(activity => activity.id === id);
-
-//   if (index === -1) {
-//     res.status(400).send('Booooo');
-//     return
-//   } else if (index >= 0) {
-//        data.splice(index, 1);
-//        }
-
-//   res.status(200).send({ data })
-//   console.log(data);
-// });
 
 // DELETE handler
 app.delete("/activities/:id", (req, res) => {
   // const deleteBody = req.body.delete
   const id = req.params.id;
-  const index = data.findIndex(activity => activity.id === id);
-
-       data.splice(index, 1);
-
-  res.status(200).send({ data })
-  console.log(data);
+  const index = data.findIndex((activity) => activity.id === id);
+  // -1 is a value returned from findIndex method
+  if (index === -1) {
+    res.status(400).send("Invalid ID");
+  }
+  data.splice(index, 1);
+  res.status(200).send({ data });
 });
-
-
-
-
-
-
-// app.delete("/api/notes/:id", function(req, res) {
-//   console.log("req params", req.params.id)
-//   const itemIndex = myArray.findIndex(({ id }) => id === req.params.id);
-//   if (itemIndex >= 0) {
-//     myArray.splice(itemIndex, 1);
-//   }
-// });
-
 
 // listens to the port 3000
 app.listen(PORT, () => {
